fa2e5aaffee3ead8dfcd1c47bee039a66c93439d
music <musicsnap@163.com>
2016-06-13 16:53:29 +0800

    页面设置

--- php/thinkphp3/Application/Admin/Controller/IndexController.class.php
+++ php/thinkphp3/Application/Admin/Controller/IndexController.class.php
@@ -5,4 +5,9 @@ class IndexController extends Controller {
     public function indexAction(){
         $this->display('index');
     }
+
+    public function mainAction(){
+        echo "hello this is a main.html";
+        die();
+    }
 }
