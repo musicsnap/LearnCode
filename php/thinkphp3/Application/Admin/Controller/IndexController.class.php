45f0774e3d07d5274b2745a6a225e6c2d3640323
music <musicsnap@163.com>
2016-06-13 16:45:03 +0800

    1

--- php/thinkphp3/Application/Admin/Controller/IndexController.class.php
+++ php/thinkphp3/Application/Admin/Controller/IndexController.class.php
@@ -3,7 +3,6 @@ namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function indexAction(){
-        echo "这是后台默认的首页!";
-        die();
+        $this->display('index');
     }
 }
