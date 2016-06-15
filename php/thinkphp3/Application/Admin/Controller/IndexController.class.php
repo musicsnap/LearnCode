1d2975323fb164170585dddf3ed84af4ac486935
music <musicsnap@163.com>
2016-06-15 18:00:33 +0800

    1

--- php/thinkphp3/Application/Admin/Controller/IndexController.class.php
+++ php/thinkphp3/Application/Admin/Controller/IndexController.class.php
@@ -2,10 +2,11 @@
 namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-
+    //控制器初始化
     public function _initialize(){
 
     }
+
     public function indexAction(){
         $this->display('index');
     }
