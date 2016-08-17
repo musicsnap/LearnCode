c9ada59310af26e415df412d4db14e4f5d054fc0
music <musicsnap@163.com>
2016-08-17 10:52:30 +0800

    1

--- php/laravel/app/Http/routes.php
+++ php/laravel/app/Http/routes.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::auth();
-
-Route::get('/home', 'HomeController@index');
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+});
 
