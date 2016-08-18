9c98fc1da639fa3addb1ee6faa20479845cdaff9
music <musicsnap@163.com>
2016-08-18 18:08:35 +0800

    1

---
+++ php/laravel/app/Http/Controllers/Admin/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    //主页
+    public function index(){
+        return "hello world!";
+    }
+
+}
